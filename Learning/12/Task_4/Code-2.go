1bb814f870c4fe6b649eaa4c15e2516ec0a185c0
jedyEvgeny <97782789+jedyEvgeny@users.noreply.github.com>
2023-02-16 12:51:44 +1000

    Update Code-2.go

--- Learning/12/Task_4/Code-2.go
+++ Learning/12/Task_4/Code-2.go
@@ -1,1 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("Log.txt")
+	if err != nil {
+		fmt.Println("Файл отсутствует")
+		panic(err)
+		return
+	}
+	defer file.Close()
+
+	info, err := file.Stat()
+	if err != nil {
+		panic(err)
+		return
+	}
+
+	if info.Size() == 0 {
+		fmt.Println("Файл пуст")
+		return
+	}
+
+	resultBytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		panic(err)
+		return
+	}
+	fmt.Println(string(resultBytes))
+}
 
