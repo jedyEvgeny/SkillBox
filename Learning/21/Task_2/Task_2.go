aed7acd311dda781322b1e22ef1d2badaa59f240
jedyEvgeny <97782789+jedyEvgeny@users.noreply.github.com>
2023-04-22 11:45:35 +1000

    Update Task_2.go

--- Learning/21/Task_2/Task_2.go
+++ Learning/21/Task_2/Task_2.go
@@ -1,1 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(wrapFunc(20, 30, func(l, m int) int { return l + m }))
+	fmt.Println(wrapFunc(20, 30, func(l, m int) int { return l * m }))
+	fmt.Println(wrapFunc(20, 30, func(l, m int) int { return l - m }))
+}
+
+func wrapFunc(inL, inM int, A func(int, int) int) int {
+	// Непонятно, что тут делать
+}
 
