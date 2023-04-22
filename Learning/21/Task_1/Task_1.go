a5895ca7bd9e4ac215ba7396c5e2146525d3ed54
jedyEvgeny <97782789+jedyEvgeny@users.noreply.github.com>
2023-04-23 09:57:04 +1000

    Update Task_1.go

--- Learning/21/Task_1/Task_1.go
+++ Learning/21/Task_1/Task_1.go
@@ -8,11 +8,8 @@ func main() {
 	var x int16
 	var y uint8
 	var z float32
-	fmt.Println("Введите три числа")
+	f := func(x int16, y uint8, z float32) float32 { return 2*float32(x) + float32(y*y) - 3/z }
+	fmt.Println("Введите три числа через клавишу Enter")
 	fmt.Scan(&x, &y, &z)
-	fmt.Println(sum(x, y, z))
-}
-
-func sum(x int16, y uint8, z float32) float32 {
-	return 2*float32(x) + float32(y*y) - 3/z
+	fmt.Println(f(x, y, z))
 }
