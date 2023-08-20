bb036e3e0a2034a7012fc9b82ee98e6fcf612bd5
jedyEvgeny <97782789+jedyEvgeny@users.noreply.github.com>
2023-08-20 22:34:58 +1000

    Update task_26.go
    Вместо os.Exit добавлена минимальная обработка ошибок. Комментарий проверяющего: "os.Exit(1) посередине вызываемой функции крайне неочевидное поведение, здесь следовало бы вернуть ошибку, что бы вызываемая функция решила что делать"

--- Learning/26/task_26.go
+++ Learning/26/task_26.go
@@ -8,41 +8,48 @@ import (
 
 func main() {
 	fmt.Println("-----Программа cat----")
-	checkAmountArgs()
+	err := checkAmountArgs()
+	if err != nil {
+		fmt.Println(err)
+		// здесь можно прописать, как обрабатывать ошибку
+	}
 	fmt.Println("Программа cat закончила выполнение работы")
 }
 
-func checkAmountArgs() {
+func checkAmountArgs() error {
 	fmt.Println("---Определяем количество аргументов---")
 	amountArgs := len(os.Args)
 	if amountArgs > 1 && amountArgs < 5 {
-		cutResult := oneArgs()
+		cutResult, err := oneArgs()
+		if err != nil {
+			fmt.Println(err)
+			// здесь можно прописать, как обрабатывать ошибку
+		}
 		if amountArgs == 4 {
 			threeArgs(cutResult)
 		}
 	} else {
-		fmt.Println("Пожалуйста, укажите три текстовых файла!")
-		os.Exit(1)
+		return fmt.Errorf("Пожалуйста, укажите три текстовых файла!")
 	}
+	return nil
 }
 
-func oneArgs() []uint8 {
+func oneArgs() ([]byte, error) {
 	fmt.Println("---Читаем содержимое файлов---")
 	var catData []byte
 	for i := 1; i < len(os.Args); i++ {
 		file, err := os.ReadFile(os.Args[i])
 		if err != nil {
-			fmt.Printf("Не удалось прочитать %d-й файл:\n", (i), err)
-			os.Exit(1)
+			return catData, fmt.Errorf("Не удалось прочитать %d-й файл:\n", (i), err)
 		}
 		fmt.Println(string(file))
 		catData = append(append(catData, "\n"...), file...)
 	}
-	return catData
+	return catData, nil
 }
 
-func threeArgs(data []uint8) {
-	err := ioutil.WriteFile(os.Args[3], []uint8(data), 0777)
+func threeArgs(data []byte) {
+	err := ioutil.WriteFile(os.Args[3], []byte(data), 0777)
 	if err != nil {
 		fmt.Println("Не удалось записать в третий файл:", err)
 		return
